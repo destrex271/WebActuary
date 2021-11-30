90948429380d546b56b54c159d64c82dbfe537fe
destrex271 <destrex271@gmail.com>
2021-11-30 08:41:58 +0530

    pip install added to .sh

--- linux/dependency.sh
+++ linux/dependency.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sudo apt install python3-pip
-pip install requests bs4 pyqt5 PyQtWebEngine selenium axe_selenium_python pysimplegui validators --user
 sudo apt-get install python3-pyqt5
-sudo apt-get install qtcreator pyqt5-dev-tools
-sudo apt-get install qttools5-dev-tools
+pip install requests bs4 pyqt5 PyQtWebEngine selenium axe_selenium_python pysimplegui validators --user
